c827157e44cdbf876dbe5767d9769e3bc692752d
MohdDhanish555 <mahammeddhanish@gmail.com>
2024-12-07 17:11:54 +0530

    feat: swagger setup, cors & helmet added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
-import { NestFactory } from "@nestjs/core";
+import { NestFactory, Reflector } from "@nestjs/core";
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from "@nestjs/swagger";
+import helmet from "helmet";
+
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(helmet());
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle("CollabDocs Api")
+    .setDescription("Collaborative Document Management System")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (_, methodKey: string) => methodKey,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup("api", app, document);
+
+  await app.listen(4000);
 }
 bootstrap();
