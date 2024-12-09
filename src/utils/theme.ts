81c7da84111d5b3ae59119af297b1987a4e74f30
MohdDhanish555 <mahammeddhanish@gmail.com>
2024-12-09 09:26:18 +0530

    feat: added new font & theme config

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,51 +1,39 @@
 import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
-  shape: {
-    borderRadius: 6,
-  },
   typography: {
-    fontFamily: "Montserrat, serif",
+    fontFamily: "Outfit, Arial, sans-serif",
     fontWeightLight: 400,
     fontWeightRegular: 500,
     fontWeightMedium: 600,
     fontWeightBold: 700,
-    body1: {
-      fontWeight: 400,
-      fontSize: 14,
-      lineHeight: "150%",
-    },
-    subtitle1: {
-      fontWeight: 400,
-      fontSize: 16,
-      lineHeight: "150%",
-    },
-    subtitle2: {
-      fontWeight: 400,
-      fontSize: 18,
-      lineHeight: "150%",
-    },
-    h2: {
-      fontWeight: 700,
-      fontSize: 30,
-      lineHeight: "150%",
-    },
-    h6: {
-      fontWeight: 500,
-      fontSize: 20,
-      lineHeight: "150%",
-    },
+    h1: { fontSize: "2.5rem", fontWeight: 700 }, // 40px
+    h2: { fontSize: "2rem", fontWeight: 600 }, // 32px
+    h3: { fontSize: "1.75rem", fontWeight: 500 }, // 28px
+    h4: { fontSize: "1.5rem", fontWeight: 500 }, // 24px
+    body1: { fontSize: "1rem", fontWeight: 400 }, // 16px
+    body2: { fontSize: "0.875rem", fontWeight: 400 }, // 14px
+    subtitle1: { fontSize: "1.25rem", fontWeight: 500 }, // 20px
+    subtitle2: { fontSize: "1.125rem", fontWeight: 500 }, // 18px
   },
   palette: {
-    primary: {
-      main: "#181C62",
+    primary: { main: "#181c62" },
+    secondary: { main: "#dc004e" },
+    error: { main: "#f44336" },
+    warning: { main: "#ff9800" },
+    info: { main: "#2196f3" },
+    success: { main: "#4caf50" },
+    background: { default: "#f5f5f5" },
+    text: { primary: "#212121", secondary: "#757575", disabled: "#9e9e9e" },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: "8px",
+          textTransform: "none",
+        },
+      },
     },
-    secondary: {
-      main: "#D71440",
-    },
-    text: {
-      primary: "#111928",
-    },
-    divider: "#e5e7eb",
   },
 });
