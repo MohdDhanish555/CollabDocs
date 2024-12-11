746d04ffae04a4ca1ba86fe1557895073714acde
MohdDhanish555 <mahammeddhanish@gmail.com>
2024-12-11 23:35:11 +0530

    feat: lottie animation added

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -1,15 +1,57 @@
-import { Container, Stack } from "@mui/material";
+import { Backdrop, Container } from "@mui/material";
 
 import DashboardHeader from "./Header";
 import Documents from "./Documents/Documents";
+import createDocAnimation from "./createDocAnimation.json";
+import { useEffect, useRef, useState } from "react";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 const Dashboard = () => {
+  const [dotLottie, setDotLottie] = useState<any>(null);
+  const [showLoader, setShowLoader] = useState(false);
+  const animationCompleteRef = useRef(false);
+
+  const dotLottieRefCallback = (dotLottie: any) => {
+    setDotLottie(dotLottie);
+  };
+
+  useEffect(() => {
+    function loopCount(event: any) {
+      const loopCount = event?.loopCount;
+      if (loopCount === 1) {
+        animationCompleteRef.current = true;
+      }
+    }
+
+    if (dotLottie) {
+      dotLottie.addEventListener("loop", loopCount);
+    }
+    return () => {
+      if (dotLottie) {
+        dotLottie.removeEventListener("loop", loopCount);
+      }
+    };
+  }, [dotLottie]);
+
   return (
     <Container sx={{ height: "100%" }}>
-      <Stack>
-        <DashboardHeader />
-        <Documents />
-      </Stack>
+      <Backdrop
+        open={showLoader}
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <DotLottieReact
+          data={createDocAnimation}
+          loop
+          dotLottieRefCallback={dotLottieRefCallback}
+          layout={{ fit: "contain", align: [50, 50] }}
+        />
+      </Backdrop>
+      <DashboardHeader
+        dotLottie={dotLottie}
+        setShowLoader={setShowLoader}
+        animRef={animationCompleteRef}
+      />
+      <Documents />
     </Container>
   );
 };
--- src/Components/Dashboard/Header.tsx
+++ src/Components/Dashboard/Header.tsx
@@ -1,20 +1,44 @@
-import React from "react";
 import { Box, Button, Typography } from "@mui/material";
 import { Add } from "@mui/icons-material";
 import { useNavigate } from "react-router";
-import http from "../../utils/http";
 import { AxiosResponse } from "axios";
+
+import http from "../../utils/http";
 import { errorToastMessage } from "../../utils/toast";
 
-const DocumentHeader = () => {
+const DocumentHeader = ({ dotLottie, setShowLoader, animRef }: any) => {
   const navigate = useNavigate();
 
   const handleStartButton = async () => {
     try {
+      setShowLoader(true);
+      if (dotLottie) {
+        dotLottie.play();
+      }
       const res: AxiosResponse = await http.post("/documents/create");
-      navigate(`/document/${res.data?.data?.id}`);
+      const data = res.data?.data;
+
+      const waitForAnimation = new Promise((resolve) => {
+        const checkAnimation = setInterval(() => {
+          if (animRef.current) {
+            clearInterval(checkAnimation);
+            resolve(true);
+          }
+        }, 100);
+      });
+
+      await waitForAnimation;
+      setShowLoader(false);
+      if (dotLottie) {
+        dotLottie.stop();
+      }
+      navigate(`/document/${data?.id}`);
     } catch (error) {
       errorToastMessage(error as Error);
+      setShowLoader(false);
+      if (dotLottie) {
+        dotLottie.stop();
+      }
     }
   };
 
