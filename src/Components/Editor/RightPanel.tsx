b78b2ef0062e5c4dfaee945abf8d22cb060825ce
MohdDhanish555 <mahammeddhanish@gmail.com>
2024-12-26 00:17:21 +0530

    feat: added real time active users & comments

---
+++ src/Components/Editor/RightPanel.tsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { Avatar, AvatarGroup, Box } from "@mui/material";
+import { deepOrange } from "@mui/material/colors";
+
+import { useAppSelector } from "../../Redux/hooks";
+import socket from "../../utils/socketService";
+import Comments from "./Comments";
+
+interface User {
+  id: string;
+  username: string;
+}
+
+const RightPanel = () => {
+  const userId = useAppSelector((state) => state.user.userId);
+  const [activeUsers, setActiveUsers] = useState<User[]>([
+    { id: userId, username: "You" },
+  ]);
+  const { id: documentId } = useParams();
+
+  useEffect(() => {
+    socket.emit("joinRoom", { documentId, userId });
+
+    socket.on("usersUpdated", (users) => {
+      setActiveUsers(users);
+    });
+
+    return () => {
+      socket.emit("leaveRoom", { documentId, userId });
+    };
+  }, [documentId, userId]);
+
+  useEffect(() => {
+    const handleBeforeUnload = () => {
+      socket.emit("leaveRoom", { documentId, userId });
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [documentId, userId]);
+
+  return (
+    <Box
+      sx={{
+        height: "100%",
+        minHeight: "450px",
+        width: "350px",
+        bgcolor: "#7091e6",
+        borderRadius: "16px",
+        p: 1.5,
+      }}
+    >
+      <Box
+        sx={{
+          // bgcolor: "lightgray",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <AvatarGroup max={4}>
+          {activeUsers.map((user) => (
+            <Avatar
+              key={user?.id}
+              sx={{ textTransform: "capitalize", bgcolor: deepOrange[500] }}
+            >
+              {user?.username?.charAt(0)}
+            </Avatar>
+          ))}
+        </AvatarGroup>
+      </Box>
+      <Comments />
+    </Box>
+  );
+};
+
+export default RightPanel;
