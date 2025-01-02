e6ef591099bc5abbc2004cc7b5000334be1e94f4
MohdDhanish555 <mahammeddhanish@gmail.com>
2025-01-03 02:41:45 +0530

    feat: socket io baseUrl updated

--- src/utils/socketService.ts
+++ src/utils/socketService.ts
@@ -1,5 +1,5 @@
 import { io, Socket } from "socket.io-client";
 
-const socket: Socket = io("http://localhost:4000");
+const socket: Socket = io(`${process.env.REACT_APP_BASE_URL}`);
 
 export default socket;
