2f63b9e8cc9a6ed87375d7c04afcab15ccc17b44
MohdDhanish555 <mahammeddhanish@gmail.com>
2025-03-20 13:32:10 +0530

    fix: socket autoconnect

--- src/utils/socketService.ts
+++ src/utils/socketService.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from "socket.io-client";
 
 const socket: Socket = io(`${process.env.REACT_APP_BASE_URL}`, {
-  autoConnect: false,
+  autoConnect: true,
 });
 
 export default socket;
